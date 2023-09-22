1ccea4ec58dfc2d96ab5b7af042cc06b915ec150
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2023-09-22 08:32:43 -0300

    Adicionando estilo pagina inicial

--- controllers/produtos-controler.js
+++ controllers/produtos-controler.js
@@ -1,33 +1,56 @@
-import { produtosServicos } from "../services/produtos-services.js"
+import { produtosServicos } from "../services/produtos-services.js";
 
-const produtos = document.querySelector('[data-product]');
+const consoles = document.querySelector("[data-console]");
+//renderiza os consoles
+const notebook = document.querySelector("[data-notebook]");
+//renderiza os note
+const eletronicos = document.querySelector("[data-eletronicos]");
+//renderiza os eletronicos em geral
 
 const novoProduto = (name, imageUrl, price) => {
-    const card = document.createElement("div")
-    const conteudo = `
+  const card = document.createElement("div");
+  const conteudo = `
     <div class="produto">
-            <img
-              src="${imageUrl}"
-              alt=""
-              width="156px"
-              height="154px"
-            />
-            <h1 class="product-name">${name}</h1>
-            <p class="preco">${price}</p>
-            <a href="{}" class="ver-produto">Ver produto</a>
-          </div>`
-    card.innerHTML = conteudo
+        <div class="produto-itens">
+        <img
+        src="${imageUrl}"
+        alt=""
+        width="145px"
+        height="145px"
+    />
+    <h1 class="product-name">${name}</h1>
+    <p class="preco">R$${price}</p>
+    <a href="{}" class="ver-produto">Ver produto</a>
+        </div>
+        
+    </div>`;
+  card.innerHTML = conteudo;
 
-    return card
-}
+  return card;
+};
 
 async function buscarProdutos() {
-    try {
-        const produtosa = await produtosServicos.listaProduto();
-        produtosa.forEach(elemento => produtos.appendChild(novoProduto(elemento.name, elemento.imageUrl, elemento.price)));
-    } catch (error) {
-        console.log("Erro ao buscar produtos:", error);
-    }
+  try {
+    const produtosa = await produtosServicos.listaProduto();
+
+    produtosa.forEach((elemento) => {
+      const card = novoProduto(
+        elemento.name,
+        elemento.imageUrl,
+        elemento.price
+      );
+
+      if (elemento.type === "console") {
+        consoles.appendChild(card);
+      } else if (elemento.type === "notebook") {
+        notebook.appendChild(card);
+      } else {
+        eletronicos.appendChild(card);
+      }
+    });
+  } catch (error) {
+    console.log("Erro ao procurar produtos disponiveis", error);
+  }
 }
 
 buscarProdutos();
